6a3689690b2499fe9201b69f543b242eac34a64e
Raluca <spatarularisaraluca@gmail.com>
2024-10-11 02:19:58 +0300

    Add toggleMenu function to script.js

--- script.js
+++ script.js
@@ -29,5 +29,12 @@ document.addEventListener('DOMContentLoaded', function() {
     
   }); 
 
-
+  function toggleMenu() {
+    var menu = document.querySelector('.menu');
+    if (menu.style.display === 'block') {
+      menu.style.display = 'none';  
+    } else {
+      menu.style.display = 'block';
+    }
+  }
   
